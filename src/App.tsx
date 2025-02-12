37cb00a5fcb1a59829de7a62a6e634fbd9c70f10
Alexander_Ovchar <azalexxx02@gmail.com>
2025-02-12 15:02:49 +0200

    Improve image and SVG display with responsive grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -63,24 +63,28 @@ function App() {
       <div className="border-2 w-full min-h-40 border-dashed border-mainText backdrop-blur-3xl rounded-md hover:bg-slate-950/5 duration-200 relative">
         <input className="w-full h-full opacity-0" type="file" accept="image/png" multiple onChange={handleImageUpload} />
         <div className="text-mainText text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-        <p>Drag and drop or click to select images</p>
-        <FaUpload className="text-2xl inline-block" />
-      </div>
-      </div>
-      {images.map((img, index) => (
-        <div key={index}>
-          <img src={img.data} alt={img.name} width={200} />
-          <p>{img.name}</p>
+          <p>Drag and drop or click to select images</p>
+          <FaUpload className="text-2xl inline-block" />
         </div>
-      ))}
-      <button onClick={convertToSvg} disabled={images.length === 0}>Convert to SVG</button>
-      {svgs.length > 0 && <button onClick={downloadAllSvgs}>Download All SVGs</button>}
-      {svgs.map((svg, index) => (
-        <div key={index}>
-          <div dangerouslySetInnerHTML={{ __html: svg.data }} />
-          <p>{svg.name}</p>
-        </div>
-      ))}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 pb-5 md:pb-16">
+        {images.map((img, index) => (
+          <div key={index} className="flex flex-col gap-2 relative group border border-black rounded-md p-1 backdrop-blur">
+            <img className="mx-auto" src={img.data} alt={img.name} width={200} />
+            <p>{img.name}</p>
+          </div>
+        ))}
+      </div>
+      <button className="flex items-center gap-2 w-fit ml-auto bg-black text-white rounded p-1" onClick={convertToSvg} disabled={images.length === 0}>Convert to SVG</button>
+      {svgs.length > 0 && <button className="flex items-center gap-2 w-fit ml-auto bg-black text-white rounded p-1" onClick={downloadAllSvgs}>Download All SVGs</button>}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 pb-5 md:pb-16">
+        {svgs.map((svg, index) => (
+          <div key={index} className="flex flex-col gap-2 relative group border border-black rounded-md p-1 backdrop-blur">
+            <div className="w-full h-full [&_svg]:w-full [&_svg]:h-full"  dangerouslySetInnerHTML={{ __html: svg.data }} />
+            <p>{svg.name}</p>
+          </div>
+        ))}
+      </div>
     </MainContainer>
   );
 }
